b2ac30159a0417569650b2e10b31c9e21283a613
mintesin <50635173+mintesin@users.noreply.github.com>
2022-09-19 05:24:46 +0530

    Update GoogleformEntryAutomation.py

--- GoogleformEntryAutomation.py
+++ GoogleformEntryAutomation.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 import time
-
+#The data is scraped from the zillow realstate company website
 header = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
